bc9b8e98e1ed8418a9f6f8c9c4ce5f2690d0a1ed
Ryan Bajzath <R.bajzath@hotmail.com>
2022-09-02 10:49:29 -0400

    Routes and Pages set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AppointmentPage from "./pages/AppointmentPage";
+import MenuPage from "./pages/MenuPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">test</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/menu" element={<MenuPage />} />
+          <Route path="/appointments" element={<AppointmentPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
