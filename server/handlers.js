040bc04b750a4678c21352aeea94ae974dd3ef87
Ryan Bajzath <R.bajzath@hotmail.com>
2022-09-05 13:17:54 -0400

    Created READ and POST endpoints/handlers

---
+++ server/handlers.js
@@ -0,0 +1,63 @@
+const { Db } = require("mongodb");
+
+// Import MongoDB key from .env
+require(`dotenv`).config();
+
+//Connect to MongoDB, following mongoDB documentation
+const { MONGO_URI } = process.env;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+const { MongoClient, ObjectId } = require("mongodb");
+const client = new MongoClient(MONGO_URI, options);
+const db = client.db("restaurant");
+
+//Get all Menu items (READ)
+const getAllMenueOptions = async (req, res) => {
+  try {
+    //connect collection and perfom method
+    const result = await db.collection("menu").find().toArray();
+    //check is result exists then send the data from mongoDB
+    result
+      ? res.status(200).json({ status: 201, data: result })
+      : res.status(404).json({ status: 404, data: "Not Found" });
+  } catch (err) {
+    res.status(500).json({ status: 500, data: err });
+  }
+};
+
+//Get Menu items by theme :param (READ)
+const getMenueByTheme = async (req, res) => {
+  //assign variable to req.params. This is whats sent from the Front-End.
+  const theme = req.param.theme;
+  try {
+    //connect collection and perfrom mongoDB mothod
+    const result = await db.collection("menu").findOne({ theme }).toArray();
+    result
+      ? res.status(200).json({ status: 200, data: result })
+      : res.status(404).json({ status: 404, data: result });
+  } catch (err) {
+    res.status(500).json({ status: 500, data: err });
+  }
+};
+
+//Create Theme (POST)
+const createTheme = async (req, res) => {
+  try {
+    const result = await db.collection("menu").insertOne(req.body);
+    return res.status(200).json({ status: 200, data: result });
+  } catch (err) {
+    res.status(500).json({ status: 500, data: err });
+  }
+};
+
+// create Appointment
+const createAppointment = async (req, res) => {};
+
+// export handlers
+module.exports = {
+  getAllMenueOptions,
+  getMenueByTheme,
+  createAppointment,
+};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ConnectionCheckOutFailedEvent } = require("mongodb");
 const app = express();
 port = process.env.PORT | 8000;
 
@@ -7,15 +8,33 @@ const {
   getAllMenueOptions,
   getMenueByTheme,
   createAppointment,
+  createTheme,
 } = require(`./handlers`);
 
 app
   .use(express.json())
   //endpoints
   .get(`/menu`, getAllMenueOptions)
-  .get(`menue/:theme`, getMenueByTheme)
-  .post(`appointments`, createAppointment)
+  .get(`/menue/:theme`, getMenueByTheme)
+  .post(`/menu`, createTheme)
+  .post(`/appointments`, createAppointment)
 
   .listen(port, () => {
     console.log(`listening on port ${port}`);
   });
+
+//Database shape can look like this;
+//Collection called menu, inside the menu collection there can be theme objects.
+//Inside those theme objects can be arrays of the items based on theme.
+
+/*
+
+menu collection
+[
+  themeName1:[themeItem1, themeItem2,themeItem3]
+  themeName2:[themeItem1, themeItem2,themeItem3]
+  themeName3:[themeItem1, themeItem2,themeItem3]
+]
+
+
+*/
