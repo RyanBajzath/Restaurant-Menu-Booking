0cdaad15cce44c4b539b73dd5d2a6f00a34b4ecc
darkmift <darkmift@gmail.com>
2022-10-06 23:52:03 +0300

    testing possible fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,24 +3,15 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-      babel: {
-        plugins: [
-          [
-            'babel-plugin-styled-components',
-            {
-              displayName: true,
-              fileName: false
-            }
-          ]
-        ]
-      }
-    })],
+  plugins: [react()],
   server: {
     port: 3000,
   },
   build: {
     outDir: '../server/public',
+    commonjsOptions: {
+      include: [/linked-dep/, /node_modules/]
+    },
     rollupOptions: {
       external: [
         /^node:.*/,
